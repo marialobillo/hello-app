a168789304778f7afd9ed667b817d444bdb1cd5e
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-03-11 11:29:35 +0100

    using props for style

--- src/index.js
+++ src/index.js
@@ -1,30 +1,24 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var App = <h1>Hello React.js</h1>;
-// var App = (
-// 	<ul>
-// 		<li>Apple</li>
-// 		<li>Mango</li>
-// 		<li>Orange</li>
-// 	</ul>
-// );
+//var App = <h1>Hello React.js</h1>;
 
-const Component = function(){
+
+
+const Component = function(props){
+	var style = {
+		color: props.color
+	};
 	return (
-		<h1>Hello React Component</h1>
+		<div style={style}>
+		<h1>{props.greeting}</h1>
+	</div>
 	);
 };
 
-// ReactDOM.render(
-// 	App,
-// 	document.getElementById('root')
-// );
-// ReactDOM.render(
-// 	<Component></Component>,
-// 	document.getElementById('root')
-// );
+
+
 ReactDOM.render(
-	<Component />,
+	<Component greeting="Hello there" color="green"/>,
 	document.getElementById('root')
 );
