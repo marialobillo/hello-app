c26f3e46cd16c23b6c6b771e3156e1288362e68f
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-03-11 10:52:37 +0100

    components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var App = React.createElement('h1', null, 'Hello React');
+var App = <h1>Hello React.js</h1>;
+// var App = (
+// 	<ul>
+// 		<li>Apple</li>
+// 		<li>Mango</li>
+// 		<li>Orange</li>
+// 	</ul>
+// );
 
+const Component = function(){
+	return (
+		<h1>Hello React Component</h1>
+	);
+};
 
+// ReactDOM.render(
+// 	App,
+// 	document.getElementById('root')
+// );
+// ReactDOM.render(
+// 	<Component></Component>,
+// 	document.getElementById('root')
+// );
 ReactDOM.render(
-	App,
+	<Component />,
 	document.getElementById('root')
 );
