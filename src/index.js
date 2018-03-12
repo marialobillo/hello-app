74894293c06132f11bbd69d30a0a1930af3142bc
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-03-12 20:02:29 +0100

    showing data

--- src/index.js
+++ src/index.js
@@ -1,35 +1,9 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var createClass = require('create-react-class');
-
-var Component = createClass({
-	getInitialState: function() {
-		return {
-			color: 'blue'
-		};
-	},
-	handleButtonClick: function() {
-		this.setState(function(prevState) {
-			return {
-				color: (prevState.color === 'blue') ? 'green' : 'blue'
-			};
-		});
-	},
-	render: function() {
-		console.log('Inside render: ', this.state.color);
-		return (
-			<div>
-				<div style={{ color: this.state.color }}>
-					<h1>{this.props.greeting}</h1>
-				</div>
-				<button onClick={this.handleButtonClick}>ClickMe</button>
-			</div>
-		);
-	}
-});
+var Show = require('./Show');
 
 
 ReactDOM.render(
-	<Component greeting="Hello World"/>,
+	<Show />,
 	document.getElementById('root')
 );
